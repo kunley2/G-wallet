0fdaf1484c96c4d24e547d25de8842a34e8e17b8
kunley2 <adewoleadekunle23@yahoo.com>
2023-07-13 15:45:49 +0100

    forgot to put a view for forget password

--- salte/urls.py
+++ salte/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     # path('activate/<uidb64/<token>', views.,name='activate_email'),
     path('login/', views.login_with_password,name='password_login'),
     path('logout/', views.logout_user,name='logout'),
-    path('password_reset/', views.lo,name='password_reset'),
-    path('password_reset_done/', auth_views.PasswordResetCompleteView(template_name='salte/user/reset_confirmation'),name='password_reset_complete'),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView(template_name='salte/user/confirm_password.html',),name='password_reset_confirm'),
+    path('password_reset/', views.forgot_password,name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetCompleteView.as_view(template_name='salte/user/reset_confirmation'),name='password_reset_complete'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='salte/user/confirm_password.html',),name='password_reset_confirm'),
     path('account/', views.register_account,name='create_account'),
     path('face/', views.face_validation,name='register'),
 ]
